89d4be563e2efa08d28cd1ed5d59131d538b7ecf
rushil-guess <76053738+rushil-guess@users.noreply.github.com>
2025-10-24 14:15:14 +0530

    Update allowedHosts for server and preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,11 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ["portfolio-2-iheq.onrender.com"], // ✅ Add here
   },
   preview: {
     host: "0.0.0.0",
     port: parseInt(process.env.PORT) || 4173,
-    allowedHosts: ["*"], // ✅ Add your Render domain here
+    allowedHosts: ["portfolio-2-iheq.onrender.com"], // keep this too
   },
 });
